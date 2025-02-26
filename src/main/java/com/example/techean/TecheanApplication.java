1b51b0f80b8c5aa606d63974df8853db9161434d
Cristian Nustes <cristiannustes27@gmail.com>
2025-02-26 11:55:19 -0500

    Creación del proyecto con Spring Boot y dependencias desde Spring Initializr

---
+++ src/main/java/com/example/techean/TecheanApplication.java
@@ -0,0 +1,13 @@
+package com.example.techean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techean/TecheanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
